8985b0f6e20af6739d66dc8f2e0c688fb544e924
Kacper Bielecki <kazjote@gmail.com>
2012-01-17 12:46:48 +0100

    Minor test change

--- spec/functional/challenge_spec.rb
+++ spec/functional/challenge_spec.rb
@@ -4,11 +4,15 @@ require File.join(File.dirname(__FILE__), '../../', 'bin/challenge')
 describe Challenge do
   let(:err) { Proc.new { fail "API request failed" } }
 
-  it "should return correct reponse" do
+  def perform_request
     with_api(Challenge) do
       get_request({}, err) do |c|
-        c.response.should == "ok"
+        yield c
       end
     end
+  end
+
+  it "should return correct reponse body" do
+    perform_request {|c| c.response.should == "ok" }
   end
 end
